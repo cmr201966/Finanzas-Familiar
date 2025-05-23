ac75f85d287a789cdf8b84248ca42ee5a0139d33
jglez1074 <jglez1074@gmail.com>
2025-05-23 16:10:10 -0400

    after merge

--- src/router/index.js
+++ src/router/index.js
@@ -1,21 +1,19 @@
-import { createRouter, createWebHistory } from 'vue-router';
-import Login from '../views/Login.vue';
-import Home from '../views/Home.vue';
-import Registrarse from '../views/Registrarse.vue';
-import Cuentas from '../views/Cuentas.vue';
+import { createRouter, createWebHistory } from "vue-router";
+import Login from "../views/Login.vue";
+import Home from "../views/Home.vue";
+import Registrarse from "../views/Registrarse.vue";
+import Cuentas from "../views/Cuentas.vue";
 
 const routes = [
-    { path: '/', name: 'home', component: Home },
-    { path: '/login', name: 'Login', component: Login },
-    { path: '/register', name: 'Registrarse', component: Registrarse },
-    { path: '/Cuentas', name: 'cuentas', component: Cuentas },
-    //{ path: '/Presupuesto', name: 'presupuesto', component: Presupuesto },
-]
+  { path: "/", name: "home", component: Home },
+  { path: "/login", name: "Login", component: Login },
+  { path: "/register", name: "Registrarse", component: Registrarse },
+  { path: "/Cuentas", name: "cuentas", component: Cuentas },
+  //{ path: '/Presupuesto', name: 'presupuesto', component: Presupuesto },
+];
 
 const router = createRouter({
-
-  history: createWebHistory(import.meta.env.BASE_URL)
-  ,
+  history: createWebHistory(import.meta.env.BASE_URL),
   routes,
 });
 
@@ -24,7 +22,7 @@ router.beforeEach((to, from, next) => {
   const isAuthenticated = false; // Cambia esta variable según la lógica de autenticación (por ejemplo, verificando un token)
 
   if (to.meta.requiresAuth && !isAuthenticated) {
-    next('/'); // Redirige a la página de login si no está autenticado
+    next("/"); // Redirige a la página de login si no está autenticado
   } else {
     next();
   }
