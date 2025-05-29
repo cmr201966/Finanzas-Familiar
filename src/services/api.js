cc768d8715db10a79dd84b81144829151a15dd31
cmr201966 <cmr201966@gmail.com>
2025-05-29 10:53:32 -0400

    cambios navbar

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,8 @@
-import axios from 'axios'
+import axios from 'axios';
 
-	const api = axios.create({
-	  baseURL: 'http://localhost:8000/api', // cambia según entorno
-	})
-
-	export default api
+export const api = axios.create({
+  baseURL: 'http://localhost:3000/api/accounts',
+  headers: {
+    'Content-Type': 'application/json'
+  }
+});
